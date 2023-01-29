d1dfbb9a2976a22882271b2e28b48b7d3bc46151
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-01-28 18:48:04 -0800

    small bug fix

--- media-app/Views/mediaPage/new.jsx
+++ media-app/Views/mediaPage/new.jsx
@@ -8,7 +8,7 @@ function New () {
   return(
   <Fragment>
      <h3 className=" text-center font-Helvetica  mt-5">Add Media Here</h3>
-    <form className="  d-flex justify-content-center mt-3">
+    <form className="  d-flex justify-content-center mt-3 p-3">
       <input type="text" className="form-edit"/>
       <button className="btn text-light btn-dark" > Add </button>
     </form>
